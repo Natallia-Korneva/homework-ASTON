048a1035cd7ced02fa4b75a3acffa0e4527f8d82
Natallia-Korneva <natalia_korneva086@icloud.com>
2025-12-12 10:05:34 +0300

    add new commit to task 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         lessonTwo lessonTwo = new lessonTwo();
         lessonTwo.printThreeWords();
+        lessonTwo.checkSumSign();
     }
 }
--- src/main/java/org/example/lessonTwo.java
+++ src/main/java/org/example/lessonTwo.java
@@ -1,10 +1,26 @@
 package org.example;
 
 public class lessonTwo {
+    //1. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple
     public void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
+
+    //2. Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми значениями,
+// которыми захотите. Далее метод должен просуммировать эти переменные, и если их сумма больше или равна 0, то вывести в консоль
+// сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
+    public void checkSumSign() {
+        int a = 10;
+        int b = -20;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
 }
 
