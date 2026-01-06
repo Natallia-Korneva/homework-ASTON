f2bf83ff4e82c9510d414a847c93608037481573
Natallia-Korneva <natalia_korneva086@icloud.com>
2026-01-06 11:27:45 +0300

    lessonSeven

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Calculator {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public double divide(int num1, int num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Деление на ноль не допускается!");
+        }
+        return (double) num1 / num2;
+    }
+}
