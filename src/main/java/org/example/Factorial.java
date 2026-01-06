e4d45efef34e809316586332c43bc81324b60aa5
Natallia-Korneva <natalia_korneva086@icloud.com>
2026-01-06 12:30:47 +0300

    lessonSevenNG

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -5,6 +5,9 @@ public class Factorial {
         if (n < 0)
             throw new IllegalArgumentException("Факториал отрицательного числа не определен");
 
+        if (n >= 20)
+            throw new ArithmeticException("Факториал большого числа не определен");
+
         long result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
